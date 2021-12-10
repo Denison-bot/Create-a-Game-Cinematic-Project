91a3720280e35e4a313c11058a0fd5f949d0eabf
Denison Percey <denisonipercey@outlook.com>
2021-12-10 09:39:03 -0400

    chopped off anakins legs

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.Playables;
 public class TriggerScript : MonoBehaviour
 {
     public PlayableDirector timeline;
-
+    public GameObject Player;
     public bool OneTimeTrigger = false;
 
     private void OnTriggerEnter(Collider other)
@@ -14,10 +14,11 @@ public class TriggerScript : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             timeline.Play();
+            Player.GetComponent<S_FPS_Move>().enabled = false;
 
             if (OneTimeTrigger == true)
             {
-                Destroy(this.gameObject);
+             
             }
 
         }
